ebb2c5f972ab8e017b5c928b85c1df43bc6cbf9d
yo_zhang <ayu30121@hotmail.com>
2019-12-16 21:21:53 +0800

    public

--- src/main/java/com/example/springsessionsecurity/controller/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/springsessionsecurity/controller/exception/GlobalControllerExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-class GlobalControllerExceptionHandler {
+public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
